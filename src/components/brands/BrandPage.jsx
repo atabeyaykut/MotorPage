4a34fedc5ebea233fc21cc9f31ef3ea91ebea3b8
kantlori <atabeywade@gmail.com>
2025-03-13 13:18:50 +0300

    ``` feat: ```

--- src/components/brands/BrandPage.jsx
+++ src/components/brands/BrandPage.jsx
@@ -45,11 +45,12 @@ const BrandPage = ({ brandName, description, categories, img }) => {
     <div className="container mx-auto px-4 py-4 pt-16">
       <div className="text-center justify-items-center mb-6 space-y-4">
         {img && <img src={`/logos/${img}`} alt="" className="max-w-[200px] h-auto max-h-[115px]" />}
-        <p className="text-7xl
-  font-thin
-  font-['Italianno',serif]
-  bg-clip-text
-  text-[#800020]">{description}</p>
+        <p className="
+          text-7xl
+          font-thin
+          font-['Italianno',serif]
+          bg-clip-text
+          text-[#800020]">{description}</p>
       </div>
 
       <div className="space-y-16">
